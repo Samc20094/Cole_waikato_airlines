42c29390e3a93753dccba54c9a4100d37a5b12fd
Samc20094 <99440010+Samc20094@users.noreply.github.com>
2023-06-20 23:45:57 +0000

    have run program inside a definition and added ticket variables that can be changed later due to user input

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-import os
-import time
+
 name_list = []
+ticket_list = []
 
 
     
 def starter():
-    Str_name = [] 
+    global Str_name
     starter = True
     while starter == True:
-        name_list.clear()
-        print("*=*" * 8)
-        # print('*=' * 14) # HAG
-        Str_name = str(input("Hello welcome to waikato airlines!, what is your name? ")).strip().title()
-        if len(Str_name) < 3 or len(Str_name)> 35:
+        try:
+            name_list.clear()
+            Str_name = str(input("Hello welcome to waikato airlines!, what is your name? ")).strip().title()
+            if len(Str_name) < 3 or len(Str_name)> 35:
+                print("invalid input")
+                continue
+            if Str_name.isalpha() == True:
+                tickets()
+        except: 
+            Str_name.isalpha() == False
             print("invalid input")
             continue
-        if Str_name.isalpha() == True:
-            print("Hello {} Where will you flying today? (aukland, wellington, waikato)".format(Str_name))
-            print('*=*=' * 9)
-        if Str_name.isalpha() == False:
-            print("invalid input")
-            continue
+            return Str_name
 
+def tickets():
+    
+    auckland_ticket = 54
+    wellington_ticket = 58
+    waikato_ticket = 59
+    ticket = str(input("Hello {} Where will you flying today? (auckland, wellington, waikato)".format(Str_name)))
+    ticket_list.append
+    if ticket == "auckland":
+        print("okay {} that will be ${}".format(Str_name, auckland_ticket))
+    elif ticket == "wellington":
+        print("okay {} that will be ${}".format(Str_name, wellington_ticket))
+    elif ticket == "waikato":
+        print("okay {} that will be ${}".format(Str_name, waikato_ticket))
+    else:
+        print("invalid input")
+        tickets()
 
+    
 if __name__ == "__main__":
-        starter()
+    starter()
+
         
